fd785400ade8dc74f090c13802de83f1a8f20af1
hyenminkim <hyenm8922@gmail.com>
2026-02-02 16:45:16 +0900

    "백엔드 프론트 엔드 설정 완료"

--- report-backend/report/src/main/java/com/report/web/controller/MainController.java
+++ report-backend/report/src/main/java/com/report/web/controller/MainController.java
@@ -1,20 +1,27 @@
 package com.report.web.controller;
 
+import com.report.web.domain.Tk_framek;
 import com.report.web.service.MainService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequiredArgsConstructor
 public class MainController {
 
     @Autowired
     MainService mainService;
-    @GetMapping("/home")
-    public  void home(Model model){
+    @GetMapping("/api/home")
+    public List<Tk_framek> home(Model model){
 
-        model.addAttribute("list",mainService.list());
+        return mainService.list();
 
 
     }
